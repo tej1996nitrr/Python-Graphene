deee0b3e38e610551ce0a1397c5010b54f08860a
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-31 21:23:07 +0530

    mutations

--- schema.py
+++ schema.py
@@ -1,12 +1,15 @@
+#%%
 import json
 
 import graphene
 from datetime import  datetime
+import uuid
 
 class User(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value = str(uuid.uuid4()))
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
+
 
 class Query(graphene.ObjectType):
     users = graphene.List(User,limit=graphene.Int())
@@ -17,29 +20,46 @@ class Query(graphene.ObjectType):
         return False
 
     def resolve_hello(self, info):
-        return "World"
+        return "World" 
 
-    def resolve_users(self, info, limit):
+    def resolve_users(self, info, limit=None):
         return [
             User(id="1",username="Fred",created_at=datetime.now()),
             User(id="2", username="Tom", created_at=datetime.now()),
             User(id="3", username="Rachel", created_at=datetime.now())
-
         ][:limit]
 
+class createUser(graphene.Mutation):
+    user = graphene.Field(User)
 
-schema = graphene.Schema(query=Query,auto_camelcase=False)
+    class Arguments:
+        username = graphene.String()
+    
+    def mutate(self,info,username):
+        user = User(username=username)
+        return createUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = createUser.Field()
+
+schema = graphene.Schema(query=Query)
 result = schema.execute( #always in camel-case
     '''
-    {
-        users (limit:1){
-            id
-            username
-            created_at
+    mutation {
+        createUser (username: "Fred"){
+            user {
+                id
+                user
+                
+            }
         }
     }
     '''
 )
-
+print(result)
 dictResult = dict(result.data.items())
 print(json.dumps(dictResult)) 
+
+
+
+# %%
